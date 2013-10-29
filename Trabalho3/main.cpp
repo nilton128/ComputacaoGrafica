99b097fc2846c24ee55e2748f1d22741680c09b3
Nilton Rodrigues <nilton128@gmail.com>
2013-10-29 00:35:36 -0200

    Terceiro

---
+++ Trabalho3/main.cpp
@@ -0,0 +1,14 @@
+/*****************************************************/
+/*         Nilton Rodrigues Pereira             */
+/*         Exercício-Programa (Trabalho 3)           */
+/*****************************************************/
+
+//Bibliotecas
+#include <gl/gl.h>
+#include <stdio.h>
+#include <glut.h>
+#include <stdlib.h>
+#include <math.h>
+
+float MOV = 0, ACRES1 = 0.09, ACRES2 = 0.5, MOVE_DIR = 0, MOVE_ESQ = 0;
+int vezDesenho = 0;
