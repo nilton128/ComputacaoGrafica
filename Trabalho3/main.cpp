92d45baa962cfae61b161e24dce61f132038cb5d
Nilton Rodrigues <nilton128@gmail.com>
2013-10-29 00:39:29 -0200

    protótipo de função

--- Trabalho3/main.cpp
+++ Trabalho3/main.cpp
@@ -10,5 +10,17 @@
 #include <stdlib.h>
 #include <math.h>
 
+void Inicializa();
+void Calcula();
+void Teclado();
+void RetanguloPrimeiro();
+void Roda(float translateX, float translateY, float rotateMove);
+void MovimentaRoda(int distInicial);
+void Desenha();
+
 float MOV = 0, ACRES1 = 0.09, ACRES2 = 0.5, MOVE_DIR = 0, MOVE_ESQ = 0;
 int vezDesenho = 0;
+
+int main(){
+
+}
